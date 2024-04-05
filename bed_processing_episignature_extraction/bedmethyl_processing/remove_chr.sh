126c9dbfea376905e4555ccfdb7f0d3bd6aaa7e0
Benjamin Huremagic <37216361+vicarioush@users.noreply.github.com>
2024-04-05 11:53:57 +0200

    Add files via upload

---
+++ bed_processing_episignature_extraction/bedmethyl_processing/remove_chr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#path to folder with bedmethyl files
+input_folder=""
+
+for input_file in "$input_folder"/*.bed; do
+    base_name=$(basename "$input_file" .bed)
+    output_file="${base_name}_noChr.bed"
+    
+    awk 'BEGIN {FS=OFS="\t"} {$1 = substr($1, 4)} 1' "$input_file" > "$output_file"
+done
+

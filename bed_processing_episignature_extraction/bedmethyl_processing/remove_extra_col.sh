842b086b1cb0f3775e5c8277c46f7b2874d5f865
Benjamin Huremagic <37216361+vicarioush@users.noreply.github.com>
2024-10-09 09:46:58 +0200

    Update remove_extra_col.sh

--- bed_processing_episignature_extraction/bedmethyl_processing/remove_extra_col.sh
+++ bed_processing_episignature_extraction/bedmethyl_processing/remove_extra_col.sh
@@ -1,3 +1,12 @@
-for file in *.bed; do
-cut -f 1-11 "$file" > temp.bed && mv temp.bed "$file"
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <input_path>"
+  exit 1
+fi
+
+input_path="$1"
+
+for file in "$input_path"/*.bed; do
+  cut -f 1-11 "$file" > "$input_path"/temp.bed && mv "$input_path"/temp.bed "$file"
 done
